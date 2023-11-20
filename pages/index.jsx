dacb934e22f551ff103f223d247219135a94d306
Ethan Ye <chiguayeshao@gmail.com>
2023-11-21 00:57:43 +0800

    add publish button

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,22 @@
 import OrderCard from "@/components/OrderCard"
+import { Button } from "@/components/ui/button"
 import useCheckNetwork from "@/hooks/useCheckNetwork"
 
 const Home = () => {
   const { error } = useCheckNetwork()
   return (
-    <div className="grid grid-cols-3 gap-8 mx-10 pt-100">
-      <OrderCard />
-      <OrderCard />
-      <OrderCard />
-      <OrderCard />
-      <OrderCard />
+    <div className="px-8 mt-10">
+      <div className="flex flex-row justify-between mb-10">
+        <div className="font-bold text-3xl">Explore Inscriptions</div>
+        <Button>PUBLISH SELL ORDER</Button>
+      </div>
+      <div className="grid grid-cols-3 gap-8">
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+        <OrderCard />
+      </div>
     </div>
   )
 }
