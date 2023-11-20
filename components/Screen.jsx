479006dcd931d5b18b2630425bd1a0c3481f39d5
0xLuki <77870611+0xLukin@users.noreply.github.com>
2023-11-21 02:43:03 +0800

    Update Screen.jsx

--- components/Screen.jsx
+++ components/Screen.jsx
@@ -11,19 +11,12 @@ export default function Screen() {
   return (
     <div className="flex items-center gap-4">
       <section title="fileType">
-        <Select defaultValue="all">
+        <Select defaultValue="pols">
           <SelectTrigger className="w-[180px] h-14 ">
             <SelectValue placeholder="Theme" />
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value="all">All</SelectItem>
-            <SelectItem value="image" disabled={true}>
-              Image
-            </SelectItem>
-            <SelectItem value="gifs" disabled={true}>
-              GIFs
-            </SelectItem>
-            <SelectItem value="text">Text</SelectItem>
+            <SelectItem value="pols">POLS</SelectItem>
           </SelectContent>
         </Select>
       </section>
