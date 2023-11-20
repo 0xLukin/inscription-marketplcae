65ae78e18157ac9367168cb5f57399ae42b77a10
0xLuki <77870611+0xLukin@users.noreply.github.com>
2023-11-21 00:38:02 +0800

    add useCheckNetwork

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,8 +3,10 @@ import Head from "next/head"
 import styles from "../styles/Home.module.css"
 import { Button } from "@/components/ui/button"
 import OrderCard from "@/components/OrderCard"
+import useCheckNetwork from "@/hooks/useCheckNetwork"
 
 const Home = () => {
+  const { error } = useCheckNetwork()
   return (
     <div className="flex flex-row gap-8 mx-10 pt-10">
       <OrderCard />
