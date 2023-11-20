62505af315e3c4a90aaab6e9675a019be392a880
0xLuki <77870611+0xLukin@users.noreply.github.com>
2023-11-21 03:19:04 +0800

    add api test

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,8 +1,10 @@
 import React from "react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import useFetchInfo from "@/hooks/fetch/useFetchInfo"
 
 export default function Search() {
+  const { data, loading, error } = useFetchInfo("https://example.com/api/data")
   return (
     <div className="flex w-full max-w-sm items-center relative">
       <Input
